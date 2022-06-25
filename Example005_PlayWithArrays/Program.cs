18f84a239f40578e4d5f07e5b73e2fc7efc9e3bf
annmark94 <annmarkova94@gmail.com>
2022-06-26 00:04:03 +0300

    Added the folder PlayWithArrays

---
+++ Example005_PlayWithArrays/Program.cs
@@ -0,0 +1,9 @@
+﻿int[] array1 = { 0, 0, 0, 0, 0 };
+int[] array2 = new int[] { 1, 1, 2, 2, 3 };
+int[] array3 = new int[5] { 1, 0, 2, 0, 3 };
+
+// foreach (int i in array1);
+// {
+//     Console.WriteLine(i);
+// }
+Array.ForEach(array3, Console.WriteLine);
